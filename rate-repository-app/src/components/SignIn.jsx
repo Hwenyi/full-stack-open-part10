7eb18b5880ce46336a6645a55e554d1b9abb7906
Lien <hwenyi@outlook.my>
2023-11-01 21:05:54 +0800

    完成表单样式

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -1,7 +1,73 @@
-import Text from './Text'
+import React from 'react'
+import { Text, TextInput, Pressable, View, StyleSheet } from 'react-native'
+import { Formik, useField } from 'formik'
+
+const initialValues = {
+  username: '',
+  password: '',
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 16,
+    backgroundColor: '#fff',
+    paddingTop: 16,
+    paddingBottom: 32,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+  },
+})
+
+const SignInForm = ({ onSubmit }) => {
+  const [usernameField, usernameMeta, usernameHelpers] = useField('username')
+  const [passwordField, passwordMeta, passwordHelpers] = useField('password')
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        onChangeText={(text) => usernameHelpers.setValue(text)}
+        value={usernameField.value}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        onChangeText={(text) => passwordHelpers.setValue(text)}
+        value={passwordField.value}
+      />
+      <Pressable style={styles.button} onPress={onSubmit}>
+        <Text style={styles.buttonText}>Sign in</Text>
+      </Pressable>
+    </View>
+  )
+}
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>
+  const onSubmit = (values) => {
+    console.log(values)
+  }
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  )
 }
 
 export default SignIn
